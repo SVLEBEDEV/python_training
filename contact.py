7b61775d0db7f61c6b6e0932ae3b9688e6a50298
Sergey Lebedev <s.v.lebedev15@yandex.ru>
2020-04-25 11:28:12 +0300

    Выполнено задание 3

---
+++ contact.py
@@ -0,0 +1,29 @@
+class Contact:
+
+    def __init__(self, First_name, Middle_name, Last_name, Nickname, Title, Company, Address,
+                 Home, Mobile, Work, Fax, E_mail, E_mail2, E_mail3, Homepage, Birthday_day, Birthday_month, Birthday_year,
+                 Anniversary_day, Anniversary_month, Anniversary_year, Address_Secondary, Home_Secondary, Notes):
+        self.First_name = First_name
+        self.Middle_name = Middle_name
+        self.Last_name = Last_name
+        self.Nickname = Nickname
+        self.Title = Title
+        self.Company = Company
+        self.Address = Address
+        self.Home = Home
+        self.Mobile = Mobile
+        self.Work = Work
+        self.Fax = Fax
+        self.E_mail = E_mail
+        self.E_mail2 = E_mail2
+        self.E_mail3 = E_mail3
+        self.Homepage = Homepage
+        self.Birthday_day = Birthday_day
+        self.Birthday_month = Birthday_month
+        self.Birthday_year = Birthday_year
+        self.Anniversary_day = Anniversary_day
+        self.Anniversary_month = Anniversary_month
+        self.Anniversary_year = Anniversary_year
+        self.Address_Secondary = Address_Secondary
+        self.Home_Secondary = Home_Secondary
+        self.Notes = Notes
