0ec2716b5244c6f96a14767c85a3a1272e4c33b1
Sergey Lebedev <s.v.lebedev15@yandex.ru>
2020-05-01 18:44:02 +0300

    Созданы кейсы: 1. test_delete_first_contact.py 2. test_delete_first_group.py 3. test_edit_first_contact.py 4. test_edit_first_group.py

---
+++ test/test_delete_first_contact.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+
+def test_add_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.delete_first()
+    app.session.logout()
--- test/test_edit_first_group.py
+++ test/test_edit_first_group.py
@@ -2,7 +2,7 @@
 from model.group import Group
 
 
-def test_add_group(app):
+def test_edit_first_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.create(Group(name="1", header="TEST_1.1", footer="TEST_1.2"))
+    app.group.edit_first(Group(name="edit", header="edit.1", footer="edit.2"))
     app.session.logout()
