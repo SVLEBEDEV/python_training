b26f1e08aa24136b48e8b9dce7834dd6babd185f
Sergey Lebedev <s.v.lebedev15@yandex.ru>
2020-05-03 15:29:40 +0300

    Произведен рефакторинг заполнения форм для групп и контактов: создано два метода form_filling в фикстурах group и contact

--- fixture/group.py
+++ fixture/group.py
@@ -13,6 +13,13 @@ class GroupHelper:
         # Нажимаем кнопку создания группы
         wd.find_element_by_name("new").click()
         # Заполняем поля
+        self.form_filling(group)
+        # Поджтверждаем создание группы
+        wd.find_element_by_name("submit").click()
+        self.return_to_group_page()
+
+    def form_filling(self, group):
+        wd = self.app.wd
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -22,9 +29,6 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # Поджтверждаем создание группы
-        wd.find_element_by_name("submit").click()
-        self.return_to_group_page()
 
     def open_group_page(self):
         wd = self.app.wd
@@ -42,14 +46,6 @@ class GroupHelper:
         self.open_group_page()
         wd.find_element_by_name('selected[]').click()
         wd.find_element_by_name('edit').click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.form_filling(group)
         wd.find_element_by_name('update').click()
         self.return_to_group_page()
