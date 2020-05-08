6671189c9560d6105cb6e709328372542ac745c3
Sergey Lebedev <s.v.lebedev15@yandex.ru>
2020-05-08 11:19:45 +0300

    Повышена интелектуальность операций логина и логаута.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,5 +5,6 @@ from model.group import Group
 def test_add_group(app):
     app.group.create(Group(name="1", header="TEST_1.1", footer="TEST_1.2"))
 
+
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
