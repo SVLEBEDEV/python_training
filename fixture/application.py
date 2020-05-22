c69c16c8f7633c8b84a5f82d31c31b07113b90d4
Sergey Lebedev <s.v.lebedev15@yandex.ru>
2020-05-22 15:19:05 +0300

    Реализованы опции задания браузера и базовой страницы через консоль.

--- fixture/application.py
+++ fixture/application.py
@@ -6,16 +6,24 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, baseURL):
+        if browser == 'firefox':
+            self.wd = webdriver.Firefox()
+        elif browser == 'chrome':
+            self.wd = webdriver.Chrome()
+        elif browser == 'ie':
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError('Не удалось распознать браузер: %s') % browser
         self.wd.implicitly_wait(1)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.baseURL = baseURL
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.baseURL)
 
     def destroy(self):
         self.wd.quit()
